0acb8ac17390f8f08fe63381cf8af9f95e2903b2
谷祖楠 <13793165851@163.com>
2017-07-06 16:14:28 +0800

    添加验证码核对接口

---
+++ src/Handlers/CheckHandler.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The file is part of Notadd
+ *
+ * @author: AllenGu<674397601@qq.com>
+ * @copyright (c) 2017, notadd.com
+ * @datetime: 17-7-6 下午3:03
+ */
+
+namespace Notadd\Alidayu\Handlers;
+
+use Notadd\Foundation\Passport\Abstracts\SetHandler as AbstractSetHandler;
+
+/**
+ * Class DataHandler.
+ */
+class CheckHandler extends AbstractSetHandler
+{
+    public function execute()
+    {
+
+    }
+}
